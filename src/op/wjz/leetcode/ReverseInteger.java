f1943552d495d495f3767d6f2f85b50eeef5792e
wenjiezhang2013 <wenjiezhang2013@gmail.com>
2014-12-20 11:50:39 -0800

    Finished Reverse Integer

---
+++ src/op/wjz/leetcode/ReverseInteger.java
@@ -0,0 +1,40 @@
+package op.wjz.leetcode;
+
+
+public class ReverseInteger
+{
+	public int reverseInteger(int value)
+	{
+		boolean negative = false;
+		
+		if(value == 0)
+			return 0;
+		else if(value < 0)
+		{
+			negative = true;
+			value *= -1;
+		}
+		
+		String valueString = Integer.toString(value);
+		
+		char[] s = valueString.toCharArray();
+		
+		StringBuilder stringBuilder = new StringBuilder();
+		
+		for (int i = s.length - 1; i>=0; i--)
+        {
+	        stringBuilder.append(s[i]);
+        }
+		int result = 0;
+		try
+        {
+			result = Integer.parseInt(stringBuilder.toString());
+        } catch (Exception e)
+        {
+        	return 0;
+        }
+		
+		
+		return  result * ( negative ? -1 :1);
+	}
+}
